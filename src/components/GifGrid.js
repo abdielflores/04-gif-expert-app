28cbb1e5b4488da7d41c8bd92a1e1bc2dc693a07
Jose Abdiel <jose.abrajan@enl.com.mx>
2022-03-06 11:09:06 -0600

    Limpieza

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { GifGridItem } from './GifGridItem';
 import { useFetchGifs } from '../hooks/useFetchGifs';
 
